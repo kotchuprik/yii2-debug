8599ed7a1cb521ba8ad4d3b6319661e69b9a75b4
Domrachev Roman <ligser@gmail.com>
2013-09-16 20:44:27 +0700

    Views rendering tab

---
+++ views/default/panels/_viewSummary.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var int $viewsCount
+ * @var string $url
+ */
+?>
+<div class="yii2-debug-toolbar-block">
+    <a href="<?= $url ?>" title="Rendered <?= $viewsCount ?>." target="_blank">
+        Rendered: <span class="label label-default"><?= $viewsCount ?></span>
+    </a>
+</div>
